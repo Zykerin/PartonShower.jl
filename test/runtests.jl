2be5c5127dc74ba377cbfc6d7b666c280fa9d2ff
Zykerin <CGPhysics@pm.me>
2025-09-28 14:28:24 -0400

    Initial test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using PartonShower
 using Test
 
-@testset "PartonShower.jl" begin
-    # Write your tests here.
+@testset "Momentum Conservation" begin
+   myEvents, sigma, error = generateEvents(1E4, 1E6)
+   showeredEvents = []
+   for ev in (myEvents)
+        newEvent = showerEvent(ev, pTmin, aSover)
+        push!(showeredEvents, newEvent)
+        totmom = [0, 0, 0, 0]
+        for p in newEvent
+            if p.status ==1
+                totmom += [p.px, p.py, p.pz, p.E]
+            end
+        end
+        @test totmom ≈ [0, 0, 0, 0] atol=0.0001
+   end
+
 end
